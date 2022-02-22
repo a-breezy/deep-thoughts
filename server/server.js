cd49a01633651157a6f6cce7d21436b02c7755cc
a.breezy <ambrose.wilkins@gmail.com>
2022-02-22 16:40:19 -0500

    add package.json and server.js updates

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const startServer = async () => {
+	// create new Apollo server and pass in schema data
+	const server = new ApolloServer({
+		typeDefs,
+		resolvers,
+		// context: authMiddleware,
+	});
+
+	// start server
+	await server.start();
+
+	// integrate apollo serve with express middleware
+	server.applyMiddleware({ app });
+
+	// log where we can test gql api
+	console.log(`Use graphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// initialize Apollo server
+startServer();
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+db.once("open", () => {
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+	});
 });
